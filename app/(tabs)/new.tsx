67fbc9afa2b9c9cd41af3b55c83b432a72ae642a
Faysal Bsata <faysalbsata@hotmail.com>
2024-07-11 14:25:00 +0400

    imagepicker

--- app/(tabs)/new.tsx
+++ app/(tabs)/new.tsx
@@ -1,17 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Image, Pressable, Text, TextInput, View } from 'react-native';
-
+import * as ImagePicker from 'expo-image-picker';
 export default function CreatePostScreen() {
   const [caption, setCaption] = useState('');
+  const [image, setImage] = useState<string | null>(null);
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+  useEffect(() => {
+    if (!image) {
+      pickImage();
+    }
+  }, []);
   return (
     <View className="p-3 items-center flex-1">
       <Image
         source={{
-          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+          uri: image!,
         }}
-        className="w-52 aspect-[3/4] rounded-lg shadow-md"
+        className="w-52 aspect-[3/4] rounded-lg shadow-md bg-slate-300"
       />
-      <Text className="mt-5 text-xl text-blue-500 font-semibold">Change</Text>
+      <Text
+        onPress={pickImage}
+        className="mt-5 text-xl text-blue-500 font-semibold"
+      >
+        Change
+      </Text>
       <TextInput
         placeholder="Write a caption..."
         className="w-full h-24 mt-5 p-3 border bg-white border-gray-200 rounded-lg"
