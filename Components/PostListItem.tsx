c8f0713424b80ad1396e2dc2e063a62291afa1b6
Faysal Bsata <faysalbsata@hotmail.com>
2024-07-11 15:41:10 +0400

    cloudinary Images

--- Components/PostListItem.tsx
+++ Components/PostListItem.tsx
@@ -1,20 +1,42 @@
-import { Image, Text, View } from 'react-native';
+import {
+  Image,
+  SafeAreaView,
+  Text,
+  useWindowDimensions,
+  View,
+} from 'react-native';
 import { AntDesign, Feather, Ionicons } from '@expo/vector-icons';
+import { AdvancedImage } from 'cloudinary-react-native';
+import { thumbnail } from '@cloudinary/url-gen/actions/resize';
+import { byRadius } from '@cloudinary/url-gen/actions/roundCorners';
+import { focusOn } from '@cloudinary/url-gen/qualifiers/gravity';
+import { FocusOn } from '@cloudinary/url-gen/qualifiers/focusOn';
+import { cld } from '@/lib/cloudinary';
 type PostListItemProps = {
   post: any;
 };
 
 const PostListItem = ({ post }: PostListItemProps) => {
+  const { width } = useWindowDimensions();
+  const image = cld.image(post.image);
+  image.resize(thumbnail().width(width).height(width));
+
+  const avatar = cld.image(post.user.avatar_url);
+  avatar.resize(
+    thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face()))
+  );
+  console.log(image);
+  const myImage = cld.image('sample');
   return (
     <View className="bg-white">
-      <View className="p-2 flex-row items-center gap-2">
-        <Image
-          source={{ uri: post.user.image_url }}
+      <View className="p-3 flex-row items-center gap-2">
+        <AdvancedImage
+          cldImg={avatar}
           className="w-12 aspect-square rounded-full"
         />
         <Text className="font-semibold">{post.user.username}</Text>
       </View>
-      <Image source={{ uri: post.image_url }} className="w-full aspect-[4/3]" />
+      <AdvancedImage cldImg={image} className="w-full aspect-[4/3]" />
       <View className="p-3">
         <View className="flex-row gap-3">
           <AntDesign name="hearto" size={20} />
