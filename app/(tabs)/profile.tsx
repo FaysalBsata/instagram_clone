b31abe46cc59120752efe3d33ce32eb3fd7e2782
Faysal Bsata <faysalbsata@hotmail.com>
2024-07-12 23:02:50 +0400

    update profile avatar

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react';
-import { Image, Pressable, TextInput, View, Text, Alert } from 'react-native';
+import { Image, Pressable, View, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { supabase } from '@/lib/supabase';
 import CustomButton from '../Components/CustomButton';
 import { useAuth } from '@/providers/AuthProvider';
 import CustomInput from '../Components/CustomInput';
+import { cld, uploadImage } from '@/lib/cloudinary';
+import { AdvancedImage } from 'cloudinary-react-native';
+import { thumbnail } from '@cloudinary/url-gen/actions/resize';
 export default function ProfileScreen() {
   const [image, setImage] = useState<string | null>(null);
+  const [remoteImage, setRemoteImage] = useState<string | null>(null);
   const [userName, setUserName] = useState<string>('');
   const [bio, setBio] = useState<string>('');
   const { user } = useAuth();
@@ -23,18 +27,28 @@ export default function ProfileScreen() {
     if (error) {
       Alert.alert(error.message);
     }
-
+    console.log(data);
     if (data) {
       setUserName(data.username);
-      setImage(data.avatar_url);
+      setRemoteImage(data.avatar_url);
       setBio(data.bio);
     }
   }
+  const updatedProfile = {
+    id: user?.id,
+    username: userName,
+    avatar_url: image,
+    bio,
+  };
   async function updateProfile() {
     if (!user) return;
+    if (image) {
+      const response = await uploadImage(image);
+      updatedProfile.avatar_url = response?.public_id;
+    }
     const { data, error } = await supabase
       .from('profiles')
-      .upsert({ id: user.id, username: userName, avatar_url: image, bio });
+      .upsert(updatedProfile);
     if (error) {
       Alert.alert(error.message);
     }
@@ -53,15 +67,27 @@ export default function ProfileScreen() {
       setImage(result.assets[0].uri);
     }
   };
+  let remoteCloudinaryImage;
+  if (remoteImage) {
+    remoteCloudinaryImage = cld.image(remoteImage);
+    remoteCloudinaryImage.resize(thumbnail().width(300).height(300));
+  }
   return (
     <View className="flex-1 p-3">
       <Pressable onPress={pickImage}>
-        <Image
-          source={{
-            uri: image!,
-          }}
-          className="w-52 aspect-square self-center rounded-full bg-slate-300"
-        />
+        {remoteImage && remoteCloudinaryImage ? (
+          <AdvancedImage
+            cldImg={remoteCloudinaryImage}
+            className="w-52 aspect-square self-center rounded-full "
+          />
+        ) : (
+          <Image
+            source={{
+              uri: image!,
+            }}
+            className="w-52 aspect-square self-center rounded-full bg-slate-300"
+          />
+        )}
       </Pressable>
       <View className="gap-12">
         <CustomInput
