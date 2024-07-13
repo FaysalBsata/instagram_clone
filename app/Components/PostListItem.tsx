aa1134f1dcddbd2923f578600ee22e7797135596
Faysal Bsata <faysalbsata@hotmail.com>
2024-07-13 22:58:49 +0400

    likes/dislikes working

--- app/Components/PostListItem.tsx
+++ app/Components/PostListItem.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import { AntDesign, Feather, Ionicons } from '@expo/vector-icons';
 import { AdvancedImage } from 'cloudinary-react-native';
 import { thumbnail } from '@cloudinary/url-gen/actions/resize';
@@ -6,11 +6,53 @@ import { focusOn } from '@cloudinary/url-gen/qualifiers/gravity';
 import { FocusOn } from '@cloudinary/url-gen/qualifiers/focusOn';
 import { cld } from '@/lib/cloudinary';
 import PostContent from './PostContent';
+import { useEffect, useState } from 'react';
+import { supabase } from '@/lib/supabase';
+import { useAuth } from '@/providers/AuthProvider';
 type PostListItemProps = {
   post: any;
 };
 
 const PostListItem = ({ post }: PostListItemProps) => {
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+  const [likes, setLikes] = useState<any>(null);
+  const { user } = useAuth();
+  useEffect(() => {
+    fetchLike();
+  }, []);
+  useEffect(() => {
+    if (isLiked) {
+      saveLike();
+    } else {
+      deleteLike();
+    }
+  }, [isLiked]);
+  const fetchLike = async () => {
+    const { data } = await supabase
+      .from('likes')
+      .select('*')
+      .eq('user_id', user?.id)
+      .eq('post_id', post.id)
+      .select();
+    if (data && data?.length > 0) {
+      setLikes(data?.[0]);
+      setIsLiked(true);
+    }
+  };
+  const saveLike = async () => {
+    if (likes) return;
+    const { data } = await supabase
+      .from('likes')
+      .insert([{ user_id: user?.id, post_id: post.id }])
+      .select();
+    setLikes(data?.[0]);
+  };
+  const deleteLike = async () => {
+    const { error } = await supabase.from('likes').delete().eq('id', likes?.id);
+    if (!error) {
+      setLikes(null);
+    }
+  };
   const avatar = cld.image(post.user.avatar_url ?? 'user_e8efbv');
   avatar.resize(
     thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face()))
@@ -29,7 +71,12 @@ const PostListItem = ({ post }: PostListItemProps) => {
       <PostContent post={post} />
       <View className="p-3">
         <View className="flex-row gap-3">
-          <AntDesign name="hearto" size={20} />
+          <AntDesign
+            name={isLiked ? 'heart' : 'hearto'}
+            color={isLiked ? 'crimson' : 'black'}
+            onPress={() => setIsLiked(!isLiked)}
+            size={20}
+          />
           <Ionicons name="chatbubble-outline" size={20} />
           <Feather name="send" size={20} />
           <Feather name="bookmark" size={20} className="ml-auto" />
